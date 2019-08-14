cc99ed7fa04db15636b5a13a2ea8a042ef44b350
dubemelchi <dubemelchi@gmail.com>
2019-08-14 19:54:37 -0400

    public folder, assets, css, js

---
+++ config/assets/js/burger.js
@@ -0,0 +1,19 @@
+const ORM = require("../../orm");
+
+const burger = {
+  select: (cb) => {
+    ORM.select("burgers", (results) => {
+      cb(results);
+    });
+  },
+  create: (column, values, cb) => {
+    ORM.create("burgers", column, values, (results) => {
+      cb(results);
+    });
+  },
+  update: (column, newVal, whereCol, whereVal, cb) => {
+    ORM.update("burgers", column, newVal, whereCol, whereVal, (results) => {
+      cb(results);
+    });
+  },
+}
