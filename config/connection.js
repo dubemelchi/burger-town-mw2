0831f8547edd6a8280b67470c535f1e8afef0d46
dubemelchi <dubemelchi@gmail.com>
2019-08-14 20:19:23 -0400

    jawsdb per pdf...

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Setup mysql connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const dotenv = require("dotenv").config();
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
  host: "localhost",
  port: 8889,
- user: "root",
- password: "password",
+ user: process.env.SERVER_ID,
+ password: process.env.SERVER_SECRET,
  database: "burger_db"
-});
+ })
+}
 
 // make connection
 connection.connect(function(err) {
